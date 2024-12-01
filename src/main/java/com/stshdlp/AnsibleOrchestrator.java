1f6ed6b38e0061567020669250f3693d9674d7fc
Tikan312 <tikanisayo@gmail.com>
2024-12-01 18:27:21 +0000

    Добавлены классы AnsibleOrchestrator и DLPManager.

---
+++ src/main/java/com/stshdlp/AnsibleOrchestrator.java
@@ -0,0 +1,7 @@
+package com.stshdlp;
+
+public class AnsibleOrchestrator {
+    public void applyConfiguration(String playbookPath) {
+        // Вызов Ansible playbook
+    }
+}
---
+++ src/main/java/com/stshdlp/DataLeakPreventionManager.java
@@ -0,0 +1,11 @@
+package com.stshdlp;
+
+public class DataLeakPreventionManager {
+    public void detectThreats() {
+        // Логика обнаружения угроз
+    }
+
+    public void respondToIncidents() {
+        // Логика реагирования на инциденты
+    }
+}
